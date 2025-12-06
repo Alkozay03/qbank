51e5e01fa60039897d704d4306ccd03a08b82dbf
AbdulRahman Musameh <aramusameh@gmail.com>
2025-12-06 20:00:13 +0400

    Add Next TS shims to silence editor missing declarations

---
+++ types/next-shims.d.ts
@@ -0,0 +1,4 @@
+// Temporary shims to satisfy TS when Next types are not picked up by the editor.
+// Next already ships types; these declarations simply silence missing-module errors.
+declare module "next/navigation";
+declare module "next/image";
